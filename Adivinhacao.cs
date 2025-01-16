aa0c5e334d73bdac336fc2ddb1e0d48ff1b2768e
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-01-16 16:14:32 -0300

    Add files via upload

---
+++ Adivinhacao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    internal class Adivinhacao
+    {
+        public static void Executar() 
+        {
+            int palpite=0, tentativasRestantes=5 , tentativas = 0;
+            Random random = new Random();
+            bool numEncontrado=false;
+            int numSecreto=random.Next(1,11);
+
+            Console.WriteLine("Digite o palpite: ");
+            int.TryParse(Console.ReadLine(), out palpite);
+
+            while (tentativasRestantes>0 && !numEncontrado) 
+            {
+                
+                tentativas++;
+                tentativasRestantes--;
+
+
+                if (palpite == numSecreto) 
+                { 
+                    numEncontrado = true;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Parabéns você achou o numero");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Tentativas feitas: {0}", tentativas);
+
+                }else if (palpite != numSecreto)
+                {
+                    Console.WriteLine("Digite outro palpilte: ");
+                    int.TryParse(Console.ReadLine(), out palpite);
+                    Console.WriteLine("Tentativas restantes: {0}", tentativasRestantes);    
+                }
+            }
+            
+            
+
+
+        }
+    }
+}
